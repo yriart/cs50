0dbd4d03b1b41808e9c4ce557c5676b9b540d2b6
Lisa Yriart <eucibius@gmail.com>
2016-01-25 17:47:21 -0500

    fix mem leak, make it strip out garbage characters from input string

--- pset2/initials.c
+++ pset2/initials.c
@@ -2,16 +2,20 @@
 #include <cs50.h>
 #include <string.h>
 
+void stripInvalidChars(int sourceStrLen, char *sourceStr, char *destStr);
 void populateInitials(int sourceStrLen, char *sourceStr, char *destStr);
 int isLowerCase(char theChar);
+int isUpperCase(char theChar);
 char toUpperCase(char theChar);
 
 int main(void) {
     char *name = GetString();
     int nameLength = strlen(name);
+    char normalizedName[nameLength + 1];
     char initials[nameLength + 1];
 
-    populateInitials(nameLength, name, initials);
+    stripInvalidChars(nameLength, name, normalizedName);
+    populateInitials(nameLength, normalizedName, initials);
 
     for (int j = 0, n = strlen(initials); j < n; j++) {
         if (isLowerCase(initials[j])) {
@@ -20,7 +24,19 @@ int main(void) {
     }
     printf("%s\n", initials);
 
+    free(name);
     return 0;
+}
+
+void stripInvalidChars(int sourceStrLen, char *sourceStr, char *destStr) {
+    int destStrIndex = 0;
+    for (int i = 0; i < sourceStrLen; i++) {
+        if (sourceStr[i] == ' ' || isLowerCase(sourceStr[i]) || isUpperCase(sourceStr[i])) {
+            destStr[destStrIndex] = sourceStr[i];
+            destStrIndex++;
+        }
+    }
+    destStr[destStrIndex] = '\0';
 }
 
 void populateInitials(int sourceStrLen, char *sourceStr, char *destStr) {
@@ -39,6 +55,15 @@ void populateInitials(int sourceStrLen, char *sourceStr, char *destStr) {
 int isLowerCase(char theChar) {
     // use ASCII table values to check case
     if (((int) theChar >= 97) && ((int) theChar <= 122)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int isUpperCase(char theChar) {
+    // use ASCII table values to check case
+    if (((int) theChar >= 65) && ((int) theChar <= 90)) {
         return 1;
     } else {
         return 0;
